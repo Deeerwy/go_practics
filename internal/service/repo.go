1db8b301dd4a521142a6f48347aeb6b68ee481ee
Deeerwy <125497941+Deeerwy@users.noreply.github.com>
2025-12-13 10:18:04 +0300

    More tests to the god of tests

---
+++ internal/service/repo.go
@@ -0,0 +1,10 @@
+package service
+
+import "fmt"
+
+type User struct{ ID int64; Email string }
+var ErrNotFound = fmt.Errorf("not found")
+
+type UserRepo interface {
+	ByEmail(email string) (User, error)
+}
