fdfbc7376952ee4cb71a56528e319d371db27889
Deeerwy <125497941+Deeerwy@users.noreply.github.com>
2026-03-31 10:14:49 +0300

    prom and graf

---
+++ internal/metrics/metrics.go
@@ -0,0 +1,41 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	HttpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path"},
+	)
+
+	HttpErrorsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_http_errors_total",
+			Help: "Total number of HTTP error responses",
+		},
+		[]string{"method", "path", "status_code"},
+	)
+
+	StudentRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_student_requests_total",
+			Help: "Total number of student requests",
+		},
+		[]string{"student_id"},
+	)
+
+	HttpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "app_http_request_duration_seconds",
+			Help:    "HTTP request duration in seconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
